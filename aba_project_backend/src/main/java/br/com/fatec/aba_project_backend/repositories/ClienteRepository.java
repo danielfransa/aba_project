d43583b4c42742c021f827b422dbf9d8e58104c2
danielfransa <daniel_fransa@hotmail.com>
2023-11-01 20:26:06 -0300

    Ajustes cadastro cliente:

--- aba_project_backend/src/main/java/br/com/fatec/aba_project_backend/repositories/ClienteRepository.java
+++ aba_project_backend/src/main/java/br/com/fatec/aba_project_backend/repositories/ClienteRepository.java
@@ -1,5 +1,10 @@
 package br.com.fatec.aba_project_backend.repositories;
 
-public class ClienteRepository {
-    //  TODO implementar repository:
+import br.com.fatec.aba_project_backend.models.entities.Clientes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Clientes, Integer> {
+
 }
